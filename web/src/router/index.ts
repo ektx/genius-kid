17173d59fdd31d0aab6f9ce85db2a880284e8693
wenlong <530675800@qq.com>
2026-01-11 15:55:22 +0800

    update

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -13,7 +13,7 @@ import RegisterView from '../views/auth/RegisterView.vue'
 import { useAuthStore } from '../store/authStore'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
